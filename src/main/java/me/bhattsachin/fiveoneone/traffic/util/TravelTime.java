9d229b5d9f2323c4f19e4434f60b7c0b6ec5c424
bhatt <sachin.bhatt@getinsured.com>
2015-04-22 18:12:08 -0700

    basic layout and beans created from xmls

---
+++ src/main/java/me/bhattsachin/fiveoneone/traffic/util/TravelTime.java
@@ -0,0 +1,49 @@
+package me.bhattsachin.fiveoneone.traffic.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import me.bhattsachin.fiveoneone.traffic.model.destination.Destinations;
+import me.bhattsachin.fiveoneone.traffic.model.destination.Destinations.Destination;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+
+public class TravelTime {
+	
+	public static final String baseUrl="http://services.my511.org/traffic/getoriginlist.aspx?token=33f74986-ede3-4e51-9755-05eb1486e4f7";
+	
+
+	public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException, JAXBException {
+		XmlMapper mapper = new XmlMapper();
+		String content = new String(Files.readAllBytes(Paths.get("/Users/sachinbhatt/dev/softwares/jaxb-ri/511/getdestinationlist.aspx.xml")));
+		
+		
+		final JAXBContext context = JAXBContext.newInstance(Destinations.class);
+
+		Unmarshaller unmarshaller = context.createUnmarshaller();
+
+		//marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+		StringWriter writer = new StringWriter();
+
+		Destinations origins = (Destinations)unmarshaller.unmarshal(new File("/Users/sachinbhatt/dev/softwares/jaxb-ri/511/getdestinationlist.aspx.xml"));
+
+		//Destinations origins = mapper.readValue(content, Destinations.class);
+		
+		for(Destination destination : origins.getDestination()){
+			System.out.println(destination);
+		}
+		
+	}
+
+}
